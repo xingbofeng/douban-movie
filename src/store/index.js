8b6ea6025de7223de65708064c4979f98207ed37
xingbofeng <503908971@qq.com>
2017-04-10 19:12:54 +0800

    feat: 加入vuex 还没配置完

---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import * as mutations from './mutations';
+import * as actions from './actions';
+import home from './modules/home';
+import createLogger from '../plugins/logger';
+
+
+Vue.use(Vuex);
+
+const debug = process.env.NODE_ENV !== 'production';
+
+export default new Vuex.Store({
+  mutations,
+  actions,
+  modules: {
+    home,
+  },
+  strict: debug,
+  plugins: debug ? [createLogger()] : [],
+});
